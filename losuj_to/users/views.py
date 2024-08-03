0ad9733b5bd2c1a74703472fc7b9c612275fe294
piotr bronikowski <pbrqsl@gmail.com>
2024-08-03 21:38:34 +0200

    saving original views

--- losuj_to/users/views.py
+++ losuj_to/users/views.py
@@ -5,10 +5,7 @@ from allauth.account.views import LoginView as AllAuthLoginView
 from allauth.account.views import SignupView as AllAuthSignupView
 from allauth.socialaccount.views import SignupView as SocialSignupView
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import (
-    LoginView,
     PasswordChangeDoneView,
     PasswordChangeView,
     PasswordResetCompleteView,
@@ -20,7 +17,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.core.exceptions import ValidationError
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView, View
 from django.views.generic.edit import CreateView
 from users.forms import (
@@ -30,63 +27,6 @@ from users.forms import (
 )
 from users.mixins import EmailConfirmationMixin
 from users.models import CustomUser
-
-
-class CustomLoginView(LoginView):
-    template_name = "users/login.html"
-    redirect_field_name = "custom_login_view"
-    redirect_authenticated_user = False
-    success_message = "You have been successfully logged in!"
-
-    class Meta:
-        model = CustomUser
-
-    def get(self, request, *args, **kwargs):
-        token = request.GET.get("token")
-        next_url = request.GET.get("next")
-        if not token:
-            return super().get(self, request, *args, **kwargs)
-        user = authenticate(request, token=token)
-        if user:
-            login(request, user)
-            if next_url is not None:
-                return redirect(next_url)
-            return redirect(reverse("home"))
-        else:
-            return redirect(reverse("login"))
-
-    def post(self, request: HttpRequest, *args: str, **kwargs: Any):
-        print(request.user)
-        return super().post(request, *args, **kwargs)
-
-    def form_valid(self, form: AuthenticationForm) -> HttpResponse:
-        user = form.get_user()
-        next = self.request.POST["next"].strip('"') or "home".strip('"')
-        self.success_url = reverse(next)
-        verified_email = has_verified_email(user, user.email)
-        if verified_email:
-            print(self.success_url)
-            messages.success(self.request, f"Successfully signed in as {user}")
-            return super().form_valid(form)
-        self.request.session["resend_email_user"] = user.email
-        messages.add_message(
-            self.request,
-            messages.ERROR,
-            "User's email has not been verified, cannot login. \
-                <a href='resend_confirmation_email/'>Resend confirmation email</a>",
-        )
-        return redirect("home")
-
-    def get_success_url(self) -> str:
-        return self.success_url
-
-
-class CustomLogoutView(View):
-    success_message = "You have been logged out!"
-
-    def get(self, request):
-        logout(request)
-        return redirect("home")
 
 
 class CustomSendEmailConfirmation(SuccessMessageMixin, EmailConfirmationMixin, View):
